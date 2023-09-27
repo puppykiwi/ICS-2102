17a2db985010b9748be5fbbe3b92d32b7aed3797
puppykiwi <johnraymwendwa04@gmail.com>
2023-09-27 20:39:22 +0300

    .

---
+++ first.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    char* name;
+    printf("Enter your name: ");
+    scanf("%s", &name);
+    printf("")
+}
---
+++ test.js
@@ -0,0 +1,6 @@
+function create_weight(a, b){
+    
+    return a * b;
+
+}
+create_weight(5, 6)
